c36479078fc37626072fa81af093821957795ca6
vivek80801 <57554760+vivek80801@users.noreply.github.com>
2021-04-26 12:18:55 +0530

    chat functionality added

--- server.ts
+++ server.ts
@@ -4,6 +4,13 @@ import { app } from "./src/app";
 
 const server = http.createServer(app);
 const port = process.env.PORT || 5000;
-const io = new Server(server);
+const io = new Server(server)
+
+io.on("connection", (socket) => {
+	console.log("user is connected")
+	socket.on("chat-message", (msg) =>{
+		socket.broadcast.emit("chat message", msg)
+	})
+})
 
 server.listen(port);
--- src/routes/chat.ts
+++ src/routes/chat.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {handleGetChat} from "../controller/services/chat";
+import { isAuth } from "../config/auth"
 
 export const chatRouter  = Router();
 
-chatRouter.get("/", handleGetChat)
+chatRouter.get("/", isAuth, handleGetChat)
