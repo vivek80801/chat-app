ffdd12a2fbdcaad8f3cd472f6893d64ea2c21f76
vivek80801 <57554760+vivek80801@users.noreply.github.com>
2021-06-01 08:29:17 +0530

    solving chat bugs

--- src/controller/services/chat.ts
+++ src/controller/services/chat.ts
@@ -1,17 +1,10 @@
 import { Request, Response } from "express";
-import { io } from "../../../server";
 
 export const handleGetChat = (req: Request, res: Response) => {
 	if (req.user) {
 		const { username }: any = req.user;
-		io.on("connection", (socket) => {
-			socket.on("new-user", (msg) => {
-				socket.broadcast.emit("new-user-joined", msg);
-			});
-			socket.on("chat-message", (msg) => {
-				socket.broadcast.emit("chat message", msg);
-			});
-		});
 		res.render("chat", { username: username });
+	} else {
+		res.redirect("/login");
 	}
 };
